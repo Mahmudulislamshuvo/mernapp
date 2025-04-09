3161902c9c2a190dfc43706f9ed6bff5f859d8e7
Mahmudul Islam <mahmudulislammern@gmail.com>
2025-04-10 05:20:42 +0700

    registration design done but problem with font family

--- src/screens/Registration/Registration.js
+++ src/screens/Registration/Registration.js
@@ -1,4 +1,11 @@
-import {ImageBackground, Text, TextInput, View} from 'react-native';
+import {
+  Button,
+  ImageBackground,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import {Singup} from './style';
 
@@ -7,20 +14,38 @@ const Registration = () => {
     <View>
       <View>
         <ImageBackground
-          source={require('../../assets/registrationIMG/image.jpg')}
+          source={require('../../assets/images/registrationIMG/image.jpg')}
           resizeMode="cover"
           style={Singup.image}
         />
         <View style={Singup.overlay}></View>
       </View>
       <View style={Singup.textContainer}>
-        <View>
+        <View style={{width: '90%'}}>
           <View style={{paddingVertical: 20}}>
             <Text style={Singup.title}>Create an account</Text>
             <Text style={Singup.titleDes}>Enter your details below</Text>
           </View>
           <View>
-            <TextInput style={Singup.input} placeholder="sHVO" />
+            <TextInput style={Singup.input} placeholder="Write Name" />
+            <TextInput
+              style={Singup.input}
+              keyboardType="email"
+              placeholder="Email"
+            />
+            <TextInput
+              keyboardType="numeric"
+              style={Singup.input}
+              placeholder="Phone number"
+            />
+          </View>
+          <TouchableOpacity
+            style={Singup.submitButton}
+            onPress={() => alert('Create Account')}>
+            <Text style={Singup.submitButtonText}>Create Account</Text>
+          </TouchableOpacity>
+          <View style={Singup.googleButton}>
+            <Text style={Singup.googleButtonText}>Sign up with Google</Text>
           </View>
         </View>
       </View>
